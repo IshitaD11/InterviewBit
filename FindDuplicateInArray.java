9317c1141ff311eb348410bf9109ac8aff0892ae
Ishita Das <ishita.das9038@gmail.com>
2024-01-30 20:52:48 +0530

    Updates

--- FindDuplicateInArray.java
+++ FindDuplicateInArray.java
@@ -9,8 +9,12 @@ public class FindDuplicateInArray {
         int ans = repeatedNumber(A);
         System.out.println(ans);
     }
-    
+
     public static int repeatedNumber(final List<Integer> A) {
+        // using slow and fast pointer method, try to find out the source of cycle/common element.
+        // slow moves 1 step a time, fast pointer moves 2 step a time, until both matches, if not matches, then duplicate is not there
+        // initialize fast=0, again move slow and fast 1 step a time, the point it is equal is the common element
+
         int slow = A.get(0);
         int fast = A.get(A.get(0));
         
