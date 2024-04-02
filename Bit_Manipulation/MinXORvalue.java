908c6999feb6d6cab830aa5566e9e6ac4e3e5eb4
Ishita Das <ishita.das9038@gmail.com>
2024-04-02 23:58:09 +0530

    Commit 2-Apr

---
+++ Bit_Manipulation/MinXORvalue.java
@@ -0,0 +1,27 @@
+package InterviewBit.InterviewBit.Bit_Manipulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MinXORvalue {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(0, 4, 7, 9));
+        MinXORvalue m = new MinXORvalue();
+        System.out.println(m.findMinXor(list));
+    }
+
+    public int findMinXor(ArrayList<Integer> A) {
+        Collections.sort(A);
+        int minXor = Integer.MAX_VALUE;
+
+        for (int i = 0; i < A.size() - 1; i++) {
+            int currentXor = A.get(i) ^ A.get(i+1);
+            minXor = Math.min(minXor, currentXor);
+        }
+
+        return minXor;
+    }
+}
